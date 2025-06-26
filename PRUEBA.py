d2933cfba2e7f505e372173f567ea630b12080ab
Stangefly <ange.vergara@duocuc.cl>
2025-06-26 14:24:45 -0400

    s

---
+++ PRUEBA.py
@@ -0,0 +1,31 @@
+usuarios= [
+    {"nombre": "Angel Vergara","sexo": "M", "contraseña": "migatitomiau",},
+
+]
+def ingresar_usuario(nombre,sexo,contraseña):
+    nuevo_usuario= {
+            "nombre":,
+            "sexo":,
+            "contraseña":
+        }
+    nuevo_usuario.append()
+
+#while true:
+    print("==================Menu Principal====================")
+    print("1.- Ingresar usuario")
+    print("2.- Buscar usuario")
+    print("3.- Eliminar usuario")
+    print("4.- Salir ")
+    opcion= int(input("Ingrese una opcion del 1 al 4"))
+
+    #match opcion:
+
+        #case 1:
+        #case 2:
+        #case 3:     
+        #case 4:
+        #case _:
+    
+
+
+    
